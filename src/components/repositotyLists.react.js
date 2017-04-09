c123b288b563026012126e6c20fcccd0378a74c4
Belinda <b.bowlyjung@gmail.com>
2017-04-09 23:25:36 +0700

    map repository name

--- src/components/repositotyLists.react.js
+++ src/components/repositotyLists.react.js
@@ -2,12 +2,20 @@ import React from 'react'
 import RepositoryListItem from './repositotyListItem.react'
 
 class RepositoryLists extends React.Component {
+    state = {
+        repositoryName: [
+            "facebook/react",
+            "vuejs/vue",
+            "angular/angular"
+        ]
+    }
     render() {
+        const repositoryItems = this.state.repositoryName.map((name) => {
+            return <RepositoryListItem repoName={name} key={name} />
+        })
         return (
             <div className="section">
-                <RepositoryListItem repoName="facebook/react"/>
-                <RepositoryListItem repoName="vuejs/vue"/>
-                <RepositoryListItem repoName="angular/angular"/>
+                { repositoryItems }
             </div>
         )
     }
